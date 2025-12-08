3172ac2cd59d235fae40123244d05fe6c09b12c8
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2025-12-08 20:40:09 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,46 @@
+-- Richtung berechnen (8 Himmelsrichtungen)
+function getDirection(heading)
+    local directions = {
+        "N", "NW", "W", "SW",
+        "S", "SE", "E", "NE"
+    }
+    local index = math.floor((heading + 22.5) / 45) % 8
+    return directions[index + 1]
+end
+
+CreateThread(function()
+    while true do
+        Wait(300)
+
+        -- Check ob Pause-Menü offen ist – wenn ja, überspringen
+        if IsPauseMenuActive() then
+            SendNUIMessage({ action = "toggleHud", show = false })
+        else
+            local hour = GetClockHours()
+            local minute = GetClockMinutes()
+            local time = string.format("%02d:%02d", hour, minute)
+
+            local ped = PlayerPedId()
+            local coords = GetEntityCoords(ped)
+            local heading = GetEntityHeading(ped)
+
+            local streetHash, _ = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
+            local street = GetStreetNameFromHashKey(streetHash)
+            local zone = GetLabelText(GetNameOfZone(coords.x, coords.y, coords.z))
+            local dir = getDirection(heading)
+
+            -- HUD anzeigen & Daten senden
+            SendNUIMessage({
+                action = "toggleHud", show = true
+            })
+
+            SendNUIMessage({
+                action = "updateHUD",
+                direction = dir,
+                street = street,
+                zone = zone,
+                time = time
+            })
+        end
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'REDLINE CITY RP | RCRP'
+description 'Direction HUD (Street & Time)'
+version '3.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+}
