21be2126953a13a628ecbf02a3f179827be7aa4f
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-02-11 21:09:54 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'REDLINE CITY RP | RCRP'
-description 'Street & Time HUD (with direction and district)'
+description 'Compass (with direction and district)'
 version '1.1 (Official Release)'
 
 client_script 'client.lua'
